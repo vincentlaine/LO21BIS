11cd5cd568a3c2a92f24a1d2c8003566c374e6ac
vincentlaine <vincent.laine.utc@gmail.com>
2014-06-15 19:19:27 +0200

    Update Cursus.h

--- Projet/Cursus.h
+++ Projet/Cursus.h
@@ -4,13 +4,14 @@
 #include <QString>
 
 enum CategorieCursus {
-    TC,Cursus,Filiere,HuTech, Mineur, autre, first=TC, last=autre
+    TC, Cursus, Branche, Filiere,HuTech, Mineur, autre, first=TC, last=autre
 };
 
 class Curs{
 private:
-
-    const Curs *parent;
+    QString* tabUV;
+    unsigned int nbUV;
+    unsigned int nbMaxUV;
     QString nom;
     CategorieCursus categorie;
     unsigned int credCS;
@@ -18,30 +19,26 @@ private:
     unsigned int credTSH;
     unsigned int credSP;
 
-    unsigned int nbUV;
-    unsigned int nbMaxUV;
-    QString* tabUV;
     Curs(const Curs &c);
     Curs& operator=(const Curs& c);
-    Curs(QString n, CategorieCursus cat, unsigned int nbCS, unsigned int nbTM, unsigned int nbTSH, unsigned int nbSP, const Curs* p=0 ) : nom(n), categorie(cat), credCS(nbCS), credTM(nbTM), credTSH(nbTSH), credSP(nbSP),nbUV(0), nbMaxUV(4), tabUV(new QString[4]), parent(p){}
+    Curs(QString n, CategorieCursus cat, int nbCS, int nbTM, int nbTSH, int nbSP) : nom(n), categorie(cat), credCS(nbCS), credTM(nbTM), credTSH(nbTSH), credSP(nbSP),nbUV(0), nbMaxUV(4), tabUV(new QString[4]){}
 
     friend class CursusManager;
 public :
     QString getNom() const {return nom;}
     CategorieCursus getCategorie() const {return categorie;}
     const QString* getTabUV()const{return tabUV;}
-    const Curs* getParent()const{return parent;}
     unsigned int getNbUV()const{return nbUV;}
     unsigned int getCredCS()const {return credCS;}
     unsigned int getCredTM()const {return credTM;}
     unsigned int getCredTSH() const {return credTSH;}
     unsigned int getCredSP() const {return credSP;}
+    void setNom(QString n){nom=n;}
     void setCredCS(unsigned int n){credCS=n;}
     void setCredTM(unsigned int n){credTM=n;}
     void setCredTSH(unsigned int n){credTSH=n;}
     void setCredSP(unsigned int n){credSP=n;}
     void setCategorie(CategorieCursus c){categorie=c;}
-    void setParent(const Curs* p){parent=p;}
     void addUV(QString u);
 };
 
